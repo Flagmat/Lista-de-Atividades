2736bbf588083fd3f8b04830f1a1da527dea2873
kauafl_ <122200500+Flagmat@users.noreply.github.com>
2025-07-26 20:02:29 -0300

    exibição de atividades

--- index.php
+++ index.php
@@ -14,7 +14,59 @@
         <h1>Lista de Atividades</h1>
         
         <div id="atividades">
+            <h3>A Serem Concluidas</h3>
+            <?php
+                $sql = $conexao->prepare("SELECT nome,descricao,data_criacao FROM atividades WHERE status = 's'");
+                $sql->execute();
+                $atividades = $sql->fetchAll(PDO::FETCH_ASSOC);
+            ?>
             
+                <table>
+                    <th>Nome</th>
+                    <th>Descrição</th>
+                    <th>Data de Criação</th>
+            <?php
+                if(count($atividades) > 0 ){
+                    foreach($atividades as $dados){ ?>      
+                        <tr>
+                            <td><?php echo $dados['nome'];?></td>
+                            <td><?php echo $dados['descricao'];?></td>
+                            <td><?php echo $dados['data_criacao'];?></td>
+                            <td><input type="checkbox" name="status" ></td>
+                        </tr>
+            <?php
+                    }
+                }
+                else{
+                    echo "<p>Atividade não encontrada. Cadastre alguma para ser exibida.</p>";
+                } ?>
+                </table>
+        </div>
+
+        <div id="atv_completas">
+            <h3>Concluidas</h3>
+            <?php
+                $sql = $conexao->prepare("SELECT nome,descricao,data_criacao FROM atividades WHERE status = 'f'");
+                $sql->execute();
+                $atividades = $sql->fetchAll(PDO::FETCH_ASSOC);
+            ?>
+                <table>
+                    <th>Nome</th>
+                    <th>Descrição</th>
+                    <th>Data de Criação</th>
+            <?php
+                if(count($atividades) > 0 ){
+                    foreach($atividades as $dados){ ?>      
+                        <tr>
+                            <td><?php echo $dados['nome'];?></td>
+                            <td><?php echo $dados['descricao'];?></td>
+                            <td><?php echo $dados['data_criacao'];?></td>
+                            <td><input type="checkbox" name="status" ></td>
+                        </tr>
+            <?php
+                    }
+                } ?>
+                </table>        
         </div>
         
         <button type="button" onclick="window.location.href='cadastra_atv.php'">Cadastrar Atividade</button>
